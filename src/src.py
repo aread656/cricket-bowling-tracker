38ea5be6f72aa51e07cedf008d41ac2ac5632413
Adam Read <aread03@qub.ac.uk>
2026-03-25 21:20:03 +0000

    Basic preprocessing

--- src/src.py
+++ src/src.py
@@ -9,3 +9,16 @@ def read_in_video():
         exit()
     return vid1
 
+def process_video(vid):
+    while True:
+        ret, frame = vid.read()
+        #ret is bool for successful frame opening
+        if not ret:
+            break
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame = cv.resize(frame, (360,640))
+        frame = cv.equalizeHist(frame)
+        cv.imshow("Video",frame)
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
