17cbcd60635ff29c8680daed2a29e0d6fbade28a
Adam Read <aread03@qub.ac.uk>
2026-04-25 16:13:11 +0100

    Refactored src into main/processor.py

---
+++ src/processor.py
@@ -0,0 +1,129 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+from delivery import Delivery
+
+class Processor:
+    def read_in_video(self,path):
+        #reads the selected video
+        vid1 = cv.VideoCapture(path)
+        if not vid1.isOpened():
+            print("Error: Video failed to open")
+            exit()
+        return vid1
+
+    def hsv_filter(self,img):
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        lower = np.array([25,100,100])
+        upper = np.array([40,255,255])
+        hsv_mask = cv.inRange(hsv,lower,upper)
+        return hsv_mask
+
+    def convolution2d(self,img,n=2):
+        #provides a 2d convolution with an n*n kernel
+        kernel = np.ones((n,n), np.float32)/(n**2)
+        img = cv.filter2D(img,-1,kernel)
+        return img
+
+    def get_background_image(self,vid):
+        #creates background image and its histogram
+        ret, frame = vid.read()
+        assert ret is not False
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame = cv.resize(frame, (1280,720))
+        frame = cv.equalizeHist(frame)
+        return frame
+
+    def background_subtraction(self,bkg,img,n=2):
+        #use a static background image to detect objects
+        #background does not include ball, so all frames
+        #with ball in them shall return a detection
+        difference = cv.absdiff(img,bkg)
+        _,mask = cv.threshold(difference,30,255,cv.THRESH_BINARY)
+        kernel = np.ones((n,n),np.uint8)
+        mask = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel)
+        mask = cv.morphologyEx(mask,cv.MORPH_CLOSE,kernel)
+        return mask
+
+    def create_cv_blob_detector(self):
+        params = cv.SimpleBlobDetector_Params()
+        params.filterByArea = True
+        params.minArea = 5
+        params.maxArea = 200
+        params.filterByColor = True
+        params.blobColor = 255
+        params.filterByCircularity = False
+        #params.minCircularity = 0.6
+        detector = cv.SimpleBlobDetector_create(params)
+        return detector
+
+    def blob_detector(self,img,detector,trajectory_array):
+        keypoints = detector.detect(img)
+        if len(keypoints)>0:
+            kp = keypoints[0]
+            x = int(kp.pt[0])
+            y = int(kp.pt[1])
+            trajectory_array.append((x,y))
+        return keypoints, trajectory_array
+    
+    def process_trajectory(self,traj):
+        #needs to stop trajectory at when traj has negative x-velocity
+        #work through each value in the array
+        #compare the current value to the previous value
+        #if negative gap, clear the remaining values
+        for v in traj:
+            shortened_traj = []
+            if traj[v] < traj[v-1]:
+                shortened_traj[v] = traj[v]
+            else:
+                return shortened_traj
+
+    def process_video(self,vid,bkg,plot=False,show=False):
+        detector = self.create_cv_blob_detector()
+        trajectory = []
+        vid.set(cv.CAP_PROP_POS_FRAMES,0)
+        while True:
+            ret, img = vid.read()
+            #ret is bool for successful frame opening
+            if not ret: break
+            #image preprocessing
+            img = cv.resize(img, (1280,720))
+            hsv = self.hsv_filter(img)
+            frame = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            frame = self.convolution2d(frame)
+            frame = cv.equalizeHist(frame)
+            #background subtraction
+            mask = self.background_subtraction(bkg,frame,3)
+            #combine with HSV filter for more effective segmentation
+            mask = cv.bitwise_and(hsv,mask)
+            #dilate the segmented areas, as small area may make detection difficult
+            kernel = np.ones((3,3),np.uint8)
+            mask = cv.dilate(mask,kernel,iterations = 2)
+            #detect blobs using cv.SimpleBlobDetector
+            keypoints,trajectory= self.blob_detector(mask,detector,trajectory)
+            #draw ball keypoint
+            output = cv.drawKeypoints(img,keypoints,np.array([]),(0,0,255),
+                                cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            #draw trajectory line
+            for i in range(1,len(trajectory)):
+                cv.line(output,trajectory[i-1],trajectory[i],(255,255,0),2)
+            #show video
+            output = cv.flip(output,1)
+            if show:
+                cv.imshow("Video",output)
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+        if plot:
+            print(trajectory)
+            xs = [p[0] for p in trajectory]
+            ys = [p[1] for p in trajectory]
+            plt.gca().invert_xaxis()
+            plt.gca().invert_yaxis()
+            plt.scatter(xs, ys)
+            plt.plot(xs, ys)
+            plt.xlabel("Distance down pitch")
+            plt.ylabel("Lateral Line")
+            plt.title("Ball trajectory")
+            plt.show()
+        delivery = Delivery(trajectory)
+        return delivery
